d470e2840c9d9436c565557b37c4acce44919de1
Tim <tico@itu.dk>
2026-08-28 16:22:38 +0200

    Assignment 3.4 in project week 1
    Added seperate functions for read and observe functionality to compartmentalize the methods.t
    Using StreamWriter code snippet to rewrite the csv file with the new data added.
    https://learn.microsoft.com/en-us/dotnet/standard/io/how-to-write-text-to-a-file
    author is retrieved from the system via Environment.UserName method.
    The observation has been appended to the csv in the repo.
    
    Co-authored-by: Beytullah beyo@itu.dk

--- Bison.CLI/Program.cs
+++ Bison.CLI/Program.cs
@@ -1,33 +1,86 @@
-﻿using System;
+﻿
+using System;
+using System.Collections;
+using System.IO;
 using System.Text;
 using Microsoft.VisualBasic.FileIO;
 
 
-var file = "/home/timhl/Projects/Bison/bison_observe_cli_db.csv";
+class Program {
 
-// Source - https://stackoverflow.com/a/8319246
-var parser = new Microsoft.VisualBasic.FileIO.TextFieldParser(file);
-parser.TextFieldType = Microsoft.VisualBasic.FileIO.FieldType.Delimited;
-parser.SetDelimiters(new string[] { "," });
+    static void Main(string[] args)
+    {
+        if (args.Length > 0) {
+            if (args[0] == "observe") {
+                if (args.Length > 1) {
+                    observe(args[1]); 
+                } else {
+                    Console.WriteLine("No message is provided");
+                }
+                
+            } else if (args[0] == "read") {
+                read();
+            }
+        } else {
+            read();
+        }
+    }
 
-parser.ReadFields();
-string author;
-string message;
-DateTimeOffset time;
+    static void read() {
+    var file = "/home/timhl/Projects/Bison/bison_observe_cli_db.csv";
 
-while (!parser.EndOfData) {
-    string[] row = parser.ReadFields();
+    // Source - https://stackoverflow.com/a/8319246
+    var parser = new Microsoft.VisualBasic.FileIO.TextFieldParser(file);
+    parser.TextFieldType = Microsoft.VisualBasic.FileIO.FieldType.Delimited;
+    parser.SetDelimiters(new string[] { "," });
+
+    parser.ReadFields();
+    string author;
+    string message;
+    DateTimeOffset time;
+
+    while (!parser.EndOfData) {
+        string[] row = parser.ReadFields();
+        
+        author = row[0];
+        message = row[1];
+        time = DateTimeOffset.FromUnixTimeSeconds(long.Parse(row[2]));
+        DateTimeOffset localTime = time.ToLocalTime();
+
+        var SB = new StringBuilder();
+
+        SB.AppendFormat("{0} @ ", author);
+        SB.Append(localTime.ToString());
+        SB.AppendFormat(": {0}", message);
+
+        Console.WriteLine(SB.ToString());
+        }
+    }
+
+    static void observe(string message) {
     
-    author = row[0];
-    message = row[1];
-    time = DateTimeOffset.FromUnixTimeSeconds(long.Parse(row[2]));
-    DateTimeOffset localTime = time.ToLocalTime();
+        long time = DateTimeOffset.UtcNow.ToLocalTime().ToUnixTimeSeconds();
+        string author = Environment.UserName;
+        
+        StringBuilder SB = new StringBuilder();
+        string csvappend = $"{author},\"{message}\",{time}";
+        //String.Format("{0},{1},{2}", author, message, time);
 
-    var SB = new StringBuilder();
+        var file = "/home/timhl/Projects/Bison/bison_observe_cli_db.csv";
+        var parser = new Microsoft.VisualBasic.FileIO.TextFieldParser(file);
 
-    SB.AppendFormat("{0} @ ", author);
-    SB.Append(localTime.ToString());
-    SB.AppendFormat(": {0}", message);
+        ArrayList csvLines = new ArrayList();
+        while (!parser.EndOfData) {
+            csvLines.Add(parser.ReadLine());
+        }
+        csvLines.Add(csvappend);
 
-    Console.WriteLine(SB.ToString());
+        using (StreamWriter outputFile = new StreamWriter(Path.Combine(file)))
+        {
+            foreach (string line in csvLines)
+                outputFile.WriteLine(line);
+        }
+
+        
+    }
 }
