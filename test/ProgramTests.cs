e3464c3a4f99388123f78376ebece3b605f21ac5
Tim <tico@itu.dk>
2026-09-08 15:57:22 +0200

    Progress on tests
    Created start of unit test UNIXTimestamps(), but it does not run currently
    
    Co-authored-by: Rasmus rajh@itu.dk

--- test/ProgramTests.cs
+++ test/ProgramTests.cs
@@ -1,10 +1,23 @@
 namespace test;
 
+using System;
+using System.Collections.Generic;
+
 public class ProgramTests
 {
     [Fact]
     public void UNIXTimestamps()
     {
+        var unixTime = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+
+        Time = DateTimeOffset
+                .FromUnixTimeSeconds(unixTime)
+                .ToLocalTime();
+
+
+
+        Assert.IsNotType<ToUnixTimeSeconds>(Time);
+        Assert.IsType<DateTimeOffset>(Time);
 
     }
 }
