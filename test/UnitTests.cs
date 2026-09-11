bcd32972ddf391f0d9618d07667be494d09daf43
Tim <tico@itu.dk>
2026-09-11 12:25:02 +0200

    Seperated tests into categorized files
    Created individual files for tests of type: unit, integraton and end-to-end

---
+++ test/UnitTests.cs
@@ -0,0 +1,16 @@
+namespace test;
+
+using System;
+using System.Collections.Generic;
+
+public class UnitTests
+{
+    [Fact]
+    public void UNIXTimestamps()
+    {
+        var unixTime = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+
+        
+
+    }
+}
