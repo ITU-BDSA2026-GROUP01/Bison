c583e7497778e63eb3bfcca978dedade769f8261
Beytullah Øzdemir <beyo@itu.dk>
2026-09-18 01:01:46 +0200

    1.a: Add Bison.CSVDBService web API with observation/comment endpoints
     Please enter the commit message for your changes. Lines starting

--- src/SimpleDB/CSVDatabase.cs
+++ src/SimpleDB/CSVDatabase.cs
@@ -1,6 +1,5 @@
 
 using CsvHelper;
-using CsvHelper.Configuration;
 using System.Globalization;
 
 namespace SimpleDB;
@@ -55,20 +54,5 @@ public sealed class CSVDatabase<T> : IDatabaseRepository<T>
         csv.NextRecord();
 
     }
-}
-
-public sealed class CheepMap : ClassMap<Cheep>
-{
-    public CheepMap()
-    {
-        Map(item => item.Author).Name("Author");
-        Map(item => item.Message).Name("Observation");
-        Map(item => item.Timestamp).Name("Timestamp");
-    }
-}
-
-public record Cheep(string Author, string Message, long Timestamp)
-{
-    public Cheep() : this(string.Empty, string.Empty, 0) { }
 }
 
