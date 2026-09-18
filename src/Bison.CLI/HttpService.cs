9a977af523979547c968352c32ae9b4ddedb5764
Tim <tico@itu.dk>
2026-09-18 12:36:35 +0200

    Added base for HttpService.cs
    - HttpService creates a (should be) reusable HttpClient
    - created template for Post and Get tasks

---
+++ src/Bison.CLI/HttpService.cs
@@ -0,0 +1,22 @@
+using System.Net.Http.Headers;
+using Microsoft.VisualBasic;
+using Bison.CLI;
+
+var client = new HttpClient();
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(
+    new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+
+static async Task Post(HttpClient client, string endpoint, Bison.CLI.Cheep cheep)
+{
+    
+}
+
+static async Task<string> GetRequest(HttpClient client, string endpoint)
+{
+    var json = await client.GetStringAsync(
+    "https://api.github.com/orgs/dotnet/repos");
+
+    return json;
+}
+
