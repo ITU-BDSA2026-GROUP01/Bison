10e586fa716ef465d6b6fa397797572346f4d7e6
ottosteinman <arau@itu.dk>
2026-09-20 19:54:54 +0200

    Fix ProposalMap property name TaxonID -> TaxonId

--- src/Bison.CLI/Proposal.cs
+++ src/Bison.CLI/Proposal.cs
@@ -18,7 +18,7 @@ namespace Bison.CLI
         {
             Map(item => item.ObservationId).Name("ObservationId");
             Map(item => item.Author).Name("Author");
-            Map(item => item.TaxonID).Name("TaxonID");
+            Map(item => item.TaxonId).Name("TaxonId");
             Map(item => item.Timestamp).Name("Timestamp");
         }
     }
