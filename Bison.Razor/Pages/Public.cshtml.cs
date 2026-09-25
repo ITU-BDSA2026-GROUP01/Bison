4aea6b59f9f8fc8e0373aa2ccb3af25bc9ceeb1a
ottosteinman <arau@itu.dk>
2026-09-25 14:50:17 +0200

    Added Razor Page template to correct folder - old legacy src folder will stay active while we refactor code
    Co-authored-by: Beytullah beyo@itu.dk
    Co-authored-by: Rasmus rajh@itu.dk
    Co-authored-by: Rats rats@itu.dk

---
+++ Bison.Razor/Pages/Public.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bison.Razor.Pages;
+
+public class PublicModel : PageModel
+{
+    private readonly ICheepService _service;
+    public List<CheepViewModel> Cheeps { get; set; }
+
+    public PublicModel(ICheepService service)
+    {
+        _service = service;
+    }
+
+    public ActionResult OnGet()
+    {
+        Cheeps = _service.GetCheeps();
+        return Page();
+    }
+}
---
+++ Bison.Razor/Pages/UserTimeline.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bison.Razor.Pages;
+
+public class UserTimelineModel : PageModel
+{
+    private readonly ICheepService _service;
+    public List<CheepViewModel> Cheeps { get; set; }
+
+    public UserTimelineModel(ICheepService service)
+    {
+        _service = service;
+    }
+
+    public ActionResult OnGet(string author)
+    {
+        Cheeps = _service.GetCheepsFromAuthor(author);
+        return Page();
+    }
+}
